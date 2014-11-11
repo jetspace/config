d6b978d144859ec509acd448e7f1a3b595244a07
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2014-11-11 16:26:44 +0100

    Create install.sh

---
+++ syslinux/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This script installs the syslinux config file...
+#
+#
+BOOT_DIR=$1
+
+cat syslinux.cfg | sed 's#INSTALLDIR#$BOOT_DIR#g' > /boot/syslinux/syslinux.cfg
+echo "SYSLINUX CONFIG UPDATE DONE"
