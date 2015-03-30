0a6f2e3986c4cfc78b848fa76040b9eb076d5d1e
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-03-30 17:58:39 +0200

    Forgot /mnt/
    *facepalm*
    
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- syslinux/install.sh
+++ syslinux/install.sh
@@ -4,5 +4,5 @@
 #
 BOOT_DIR=$1
 
-cat config/syslinux/syslinux.cfg | sed "s#INSTALLDIR#$BOOT_DIR#g" #> /boot/syslinux/syslinux.cfg
+cat config/syslinux/syslinux.cfg | sed "s#INSTALLDIR#$BOOT_DIR#g" | tee /mnt/boot/syslinux/syslinux.cfg
 echo "SYSLINUX CONFIG UPDATE DONE; Boot set to: $BOOT_DIR"
